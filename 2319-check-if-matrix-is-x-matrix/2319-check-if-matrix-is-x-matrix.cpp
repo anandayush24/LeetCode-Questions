d0ccf7f7443f9826bc1fdbd0722f25cbbf3d6834
Ayush Anand <69110785+anandayush24@users.noreply.github.com>
2022-11-28 00:35:44 +0530

    Time: 96 ms (31.93%), Space: 16.4 MB (44.06%) - LeetHub

---
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(((i+j)==n-1) || (i==j)){
+                    if(mat[i][j]==0){
+                        return false;
+                    }
+                }
+                else{
+                    if(mat[i][j]!=0){
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+};
