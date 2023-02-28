d13416cd02304ca3e61c7af5af30406e53cc01c3
Ayush Anand <69110785+anandayush24@users.noreply.github.com>
2023-03-01 03:59:51 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -36,7 +36,7 @@ public:
         for(int i=0; i<n; i++){
             sum+= arr[i];
         }
-        if(sum%2==1){
+        if(sum&1){
             return 0;
         }
         int target= sum/2;
