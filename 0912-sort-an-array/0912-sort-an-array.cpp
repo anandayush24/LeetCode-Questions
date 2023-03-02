70c7006bcd446fc907582e2ff4ff0b6e1f71fc45
Ayush Anand <69110785+anandayush24@users.noreply.github.com>
2023-03-02 06:35:05 +0530

    Time: 189 ms (67.17%), Space: 68.6 MB (36.30%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto itr: nums){
+            pq.push(itr);
+        }
+        vector<int> ans;
+        while(!pq.empty()){
+            int temp= pq.top();
+            ans.push_back(temp);
+            pq.pop();
+        }
+        
+        return ans;
     }
 };
