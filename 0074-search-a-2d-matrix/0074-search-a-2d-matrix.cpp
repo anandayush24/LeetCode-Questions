cda097c1964f34ebfa1582ae2b17845c824ebb8d
Ayush Anand <69110785+anandayush24@users.noreply.github.com>
2023-08-08 00:16:15 +0530

    Time: 3 ms (82.89%), Space: 9.5 MB (52.82%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,32 +1,43 @@
 class Solution {
-private:
-    bool rowSearch(vector<vector<int>>& matrix, int target, int row){
-        int start=0, end=matrix[row].size()-1;
-        while(start <= end){
-            int mid = (start+end)/2;
-            if(matrix[row][mid] == target){ return true; }
-            if(matrix[row][mid] > target){ end = mid-1; }
-            if(matrix[row][mid] < target){ start = mid+1; }   
-        }
-        return false;
-    }
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (!matrix.size() || !matrix[0].size()) return false;
-        int start=0, end=matrix.size()-1;
+    bool solution(vector<int>& arr, int target){
+        int n= arr.size();
+        int low=0, high= n-1;
         
-        while(start < end){
-            int mid = (start+end)/2;
-            if(matrix[mid].front() == target) return true;
-            else if(matrix[mid].back() < target){ start = mid+1; }
-            else if(matrix[mid].front() > target){ end = mid-1;}
+        while(low<=high){
+            int mid= (low+high)/2;
+            
+            if(arr[mid]==target){
+                return true;
+            }
+            else if(arr[mid]>target){
+                high= mid-1;
+            }
             else{
-                start = mid;
-                break;
+                low= mid+1;
             }
         }
         
-        bool k = rowSearch(matrix, target, start);
-        return k;
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m= matrix.size();
+        int left=0, right= m-1;
+        
+        while(left<=right){
+            int mid= (left+right)/2;
+            
+            if(matrix[mid].front()<=target && matrix[mid].back()>= target){
+                return solution(matrix[mid], target);
+            }
+            else if(matrix[mid].front()> target){
+                right= mid-1;
+            }
+            else{
+                left= mid+1;
+            }
+        }
+        
+        return false;
     }
 };
